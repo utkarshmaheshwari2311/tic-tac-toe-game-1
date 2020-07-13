8009005aa114bb3f57b34b43dd1b13557c25d47f
utkarshmaheshwari2311 <utkarshm.ec18@rvce.edu.in>
2020-07-13 21:14:43 +0530

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("hello  world")
+for i in range (1,6):
+    print (i)
+    
---
+++ intro.py
@@ -0,0 +1,12 @@
+usn = [155,161,173]
+for i in usn:
+   print (i)
+numbers = [99,98,97,96,95,94]
+number = set(numbers)
+print(numbers)
+import random
+print(random.choice(usn))
+print(random.choices(usn))
+print(sorted(numbers))
+name = input("What is your good name: ")
+print(name)
